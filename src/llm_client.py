b3c6ab9ce13586ea4401233fabbe5a0a909dcb58
Justin <cornell47@marshall.edu>
2026-03-24 22:12:56 -0400

    Update llm_client.py

--- src/llm_client.py
+++ src/llm_client.py
@@ -1,1 +1,19 @@
 #llm code, using Cohere Command A
+import cohere
+import os
+
+class LLMClient:
+    def __init__(self):
+        
+      #Must set client key before hand, showing in requirements doc
+      
+      self.client = cohere.Client(os.getenv("COHERE_API_KEY"))
+
+    def generate(self, prompt):
+        response = self.client.generate(
+            model="command-a",
+            prompt=prompt,
+            max_tokens=200,
+            temperature=0
+        )
+        return response.generations[0].text.strip()
